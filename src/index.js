c0633d4ec9c1feb7856dce05c1bdf6a324426ba3
Juan <jbojato@uninorte.edu.co>
2021-11-13 15:21:49 -0500

    feat(deployment): deploy app to firebase

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import $ from "jquery";
 import Popper from "popper.js";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-
+import firebaseApp from "../src/lib/firebase";
 import App from "./App";
 
 ReactDOM.render(
