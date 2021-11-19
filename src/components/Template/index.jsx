36dddbd62ddc8a5fee010e84d1d02575b58ee9d6
Juan <jbojato@uninorte.edu.co>
2021-11-19 01:06:03 -0500

    feat(navbar)

--- src/components/Template/index.jsx
+++ src/components/Template/index.jsx
@@ -17,17 +17,6 @@ export const Template = ({ children }) => {
       <div className="navbar nav-sections">
         <nav className="navbar navbar-expand-lg">
           <div className="container-fluid">
-            <button
-              class="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNav"
-              aria-controls="navbarNav"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
             <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav">
                 <li className="nav-item">
