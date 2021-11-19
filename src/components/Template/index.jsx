5b7837d19c727b329988a821efe9a432455d75e3
Juan <jbojato@uninorte.edu.co>
2021-11-19 00:52:12 -0500

    feat(footer)

--- src/components/Template/index.jsx
+++ src/components/Template/index.jsx
@@ -96,6 +96,48 @@ export const Template = ({ children }) => {
           </Link>
         </div>
       </div>
+      <footer>
+        <div className="wrapper-content">
+          <div className="ubication">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3942.0423823378032!2d-75.62619878521451!3d8.875646693627305!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e5bd5d9cbf211c5%3A0xc2a302338f8b0644!2sPublic%20Market%20Cienaga%20de%20Oro!5e0!3m2!1sen!2sco!4v1637298698868!5m2!1sen!2sco"
+              style={{ border: 0 }}
+              width="450"
+              height="350"
+              allowfullscreen=""
+              loading="lazy"
+            ></iframe>
+          </div>
+          <div className="contact-info">
+            <h2>Contactenos</h2>
+            <hr />
+            <ul>
+              <li>mercadocienagadeoro@gmail.com</li>
+              <li>Ciénaga de Oro, Cordoba</li>
+            </ul>
+            <ul>
+              <li>Horarios: Lunes a viernes 5:00 AM - 4:00 pm</li>
+              <li>Festivos: 6:00 am - 3:00 pm</li>
+            </ul>
+          </div>
+          <div className="footer-nav">
+            <h2>Contenido destacado</h2>
+            <hr />
+            <a aria-current="page" href="/">
+              Inicio
+            </a>
+
+            <a href="/nosotros">Nosotros</a>
+
+            <a href="/nuestraorg">Nuestra organizacion</a>
+
+            <a href="/manuales">Manuales</a>
+
+            <a href="/equipo">¿Quieres hacer parte de nuestro equipo?</a>
+          </div>
+        </div>
+      </footer>
+      <div style={{ height: "40px", backgroundColor: "#ee731a" }}></div>
     </main>
   );
 };
