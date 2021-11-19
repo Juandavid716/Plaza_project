930a71b0bbc2ed9ef2d9f432cd5d8e1de0206036
Juan <jbojato@uninorte.edu.co>
2021-11-18 23:29:42 -0500

    feat(manuales)

--- src/components/Manuales/index.jsx
+++ src/components/Manuales/index.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Template } from "../Template";
-import manualAL from "../../documentos/ManualAL.pdf";
+import Manual1 from "../../documentos/Manual1.pdf";
+import Manual2 from "../../documentos/Manual2.pdf";
+import Manual3 from "../../documentos/Manual3.pdf";
 import manual1 from "../../images/manual1.PNG";
+import manual2 from "../../images/manual2.PNG";
+import manual3 from "../../images/manual3.PNG";
 import "./manuales.css";
 import { Link } from "react-router-dom";
 
@@ -30,34 +34,34 @@ export const Manuales = () => {
             Manuales
           </h3>
           <div className="manuales">
-            <div className="col-sm-3">
+            <div className="col-sm-3" style={{ marginTop: "20px" }}>
               <Link
                 className="manualImage"
-                to={manualAL}
+                to={Manual1}
                 target="_blank"
                 download
               >
                 <img className="image_manual" src={manual1} alt="manual 1" />
               </Link>
             </div>
-            <div className="col-sm-3">
+            <div className="col-sm-3" style={{ marginTop: "20px" }}>
               <Link
                 className="manualImage"
-                to={manualAL}
+                to={Manual2}
                 target="_blank"
                 download
               >
-                <img className="image_manual" src={manual1} alt="manual 1" />
+                <img className="image_manual" src={manual2} alt="manual 1" />
               </Link>
             </div>
-            <div className="col-sm-3">
+            <div className="col-sm-3" style={{ marginTop: "20px" }}>
               <Link
                 className="manualImage"
-                to={manualAL}
+                to={Manual3}
                 target="_blank"
                 download
               >
-                <img className="image_manual" src={manual1} alt="manual 1" />
+                <img className="image_manual" src={manual3} alt="manual 1" />
               </Link>
             </div>
           </div>
